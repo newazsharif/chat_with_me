ec3176b5d6e3e55e76b8ce6bd7dd4bdd89e46524
Newaz Sharif <newaz.sharif@hotmail.com>
2018-12-20 09:45:18 +0000

    confirm password added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,25 @@ class UsersController < ApplicationController
   end
   def create
     
-    @user = User.new(user_params)
-    begin
-      @user.transaction do
-        @user.save!
-        session[:user_id] = @user.id
-        session[:username] = @user.username
-        flash[:success] = "User created successfully"
-        redirect_to root_path
-      end
-    rescue Exception => e
+    if(params[:user][:confirm_password] != user_params[:password])
       
-      flash[:error] = e.message
+      flash[:error] = "Password and Confirm password have to be same"
       redirect_to new_users_path
+    else
+      @user = User.new(user_params)
+      begin
+        @user.transaction do
+          @user.save!
+          session[:user_id] = @user.id
+          session[:username] = @user.username
+          flash[:success] = "User created successfully"
+          redirect_to root_path
+        end
+      rescue Exception => e
+        
+        flash[:error] = e.message
+        redirect_to new_users_path
+      end
     end
   end
   
